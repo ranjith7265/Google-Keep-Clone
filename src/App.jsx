1765f25db7969218429863f4d3ce56412a51082e
Ranjith K <smartranjithvip@gmail.com>
2024-09-23 19:38:10 +0530

    Prettier fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import "./App.css";
 import Header from "./components/layout/Header";
 import SideMenu from "./components/layout/SideMenu";
 import Notes from "./components/pages/Notes/Notes";
@@ -11,43 +10,44 @@ import Trash from "./components/pages/Trash";
 import Labels from "./components/pages/Labels/Labels";
 import CreateLabel from "./components/pages/Labels/CreateLabel";
 import { editState } from "./store/keepSlice";
+import "./App.css";
 
 function App() {
-	const dispatch = useDispatch();
-	const [showMenu, setShowMenu] = useState(true);
-	const [searchValue, setSearchValue] = useState("");
-	const theme = useSelector((state) => state.theme);
-	const edit = useSelector((state) => state.edit);
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(true);
+  const [searchValue, setSearchValue] = useState("");
+  const theme = useSelector((state) => state.theme);
+  const edit = useSelector((state) => state.edit);
 
-	const handleMenuClick = () => {
-		setShowMenu(!showMenu);
-	};
+  const handleMenuClick = () => {
+    setShowMenu(!showMenu);
+  };
 
-	const getSearchValue = (value) => {
-		setSearchValue(value);
-	};
-	return (
-		<Router>
-			<div style={{ position: "relative" }}>
-				<Header onMenuClick={handleMenuClick} getSearchValue={getSearchValue} />
-				<div className={`hero-container ${theme && "dark-theme"}`}>
-					<SideMenu showMenu={showMenu} />
-					<Routes>
-						<Route path="/" element={<Notes searchValue={searchValue} />} />
-						<Route path="/reminder" element={<Reminder />} />
-						<Route path="/archive" element={<Archive />} />
-						<Route path="/labels/" element={<Labels />} />
-						<Route path="/labels/:label" element={<CreateLabel />} />
-						<Route path="/trash" element={<Trash />} />
-					</Routes>
-				</div>
-				<div
-					className={`${edit ? "overlay" : ""}`}
-					onClick={() => dispatch(editState())}
-				></div>
-			</div>
-		</Router>
-	);
+  const getSearchValue = (value) => {
+    setSearchValue(value);
+  };
+  return (
+    <Router>
+      <div style={{ position: "relative" }}>
+        <Header onMenuClick={handleMenuClick} getSearchValue={getSearchValue} />
+        <div className={`hero-container ${theme && "dark-theme"}`}>
+          <SideMenu showMenu={showMenu} />
+          <Routes>
+            <Route path="/" element={<Notes searchValue={searchValue} />} />
+            <Route path="/reminder" element={<Reminder />} />
+            <Route path="/archive" element={<Archive />} />
+            <Route path="/labels/" element={<Labels />} />
+            <Route path="/labels/:label" element={<CreateLabel />} />
+            <Route path="/trash" element={<Trash />} />
+          </Routes>
+        </div>
+        <div
+          className={`${edit ? "overlay" : ""}`}
+          onClick={() => dispatch(editState())}
+        ></div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.css'
-import store, {persistor} from './store/store.js'
-import { Provider } from 'react-redux'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+import store, { persistor } from "./store/store.js";
+import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
